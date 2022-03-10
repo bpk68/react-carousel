a889fa8b96832aa1cb4caf29cea49bf3ee134728
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2022-03-10 08:59:30 +0000

    initial code base checkin

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import Carousel from './carousel';
+
+function App() {
+  return (
+    <div className="2xl:container 2xl:mx-auto 2xl:px-0 py-3 px-10">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
